fa9035bd0de75eec9fbdca725ad031e344000651
Patricia Huancaya <patricia_huancaya@glic.com>
2021-05-22 11:40:25 -0400

    Day 2 got the server.js set up for the index.ejs page

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/users', routes.users);
+
+//index
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+        
+})
 
 app.listen(3000, ()=>{
     console.log("I am listening");

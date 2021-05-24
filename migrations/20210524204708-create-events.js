eed4e649f93c38ad2954780ace3fc498228f8f7a
Patricia Huancaya <patricia_huancaya@glic.com>
2021-05-24 16:49:27 -0400

    Day 3 sequelize migration for events table

---
+++ migrations/20210524204708-create-events.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        type: Sequelize.UUID
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      startTime: {
+        type: Sequelize.TIME
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      endTime: {
+        type: Sequelize.TIME
+      },
+      timeZone: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Events');
+  }
+};
---
+++ models/events.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Events extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Events.init({
+    uuid: DataTypes.UUID,
+    title: DataTypes.STRING,
+    location: DataTypes.STRING,
+    startDate: DataTypes.DATE,
+    startTime: DataTypes.TIME,
+    endDate: DataTypes.DATE,
+    endTime: DataTypes.TIME,
+    timeZone: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Events',
+  });
+  return Events;
+};

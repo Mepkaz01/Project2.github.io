1829bf49deee4fb4a206e5d2a6dbc37886d53b92
Patricia Huancaya <patricia_huancaya@glic.com>
2021-05-24 13:45:04 -0400

    Day 3 added events show.ejs

--- models/events.js
+++ models/events.js
@@ -2,20 +2,20 @@ const events = [
     {
         eventId: "1",
         uuid: "001",
-        Title: "C4G Class",
+        title: "C4G Class",
         Location: "Zoom Meeting",        
         startDate: "2021-05-24",
         startTime: "10:00 AM",
         endDate: "2021-05-24",
         endTime: "6:00 PM",
-        timezone: "EST",
+        timeZone: "EST",
         calendarId: "01"
     },
 
     {
         eventId: "2",
         uuid: "001",
-        Title: "Doctor's Appointment",
+        title: "Doctor's Appointment",
         Location: "Doc's Office",        
         startDate: "2021-05-25",
         startTime: "9:00 AM",

8f02381627d9e0a19334bda47779c2b4c2ef3956
Patricia Huancaya <patricia_huancaya@glic.com>
2021-05-25 20:54:43 -0400

    Day 4 created my css file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const app = express();
 const routes = require('./routes');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
